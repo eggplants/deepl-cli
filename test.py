5385a1dd75dee36a3f477450c1a1019daf5af463
haruna <w10776e8w@yahoo.co.jp>
2020-07-13 12:20:37 +0900

    refactor

--- test.py
+++ test.py
@@ -10,8 +10,10 @@ from selenium.webdriver.chrome.options import Options
 class DeepLCLIArgCheckingError(Exception):
     pass
 
-def usage():
-    print(
+class DeepLCLI:
+
+    def usage(self):
+        print(
         'SYNTAX:',
         '    $ stdin | python3 test.py <from:lang>:<to:lang>',
         'USAGE',
@@ -25,56 +27,66 @@ def usage():
         '    $ sudo apt update && sudo apt -f install -y',
         '    $ pip install selenium',
         sep="\n"
-)
+    )
 
-# <fr:lang> ::= {auto, ja, en, de, fr, es, pt, it, nl, pl, ru, zh}
-fr_langs = {'auto', 'ja', 'en', 'de', 'fr', 'es', 'pt', 'it', 'nl', 'pl', 'ru', 'zh'}
-# <to:lang> ::= <fr:lang> - {auto}
-to_langs = fr_langs - {'auto'}
-# if `$ deepl-cli`
-if sys.stdin.isatty() and len(sys.argv) == 1:
-    usage()
-    exit(0)
-# raise err if stdin is empty
-if sys.stdin.isatty():
-    raise DeepLCLIArgCheckingError('stdin seems to be empty.')
-# raise err if arity != 1
-num_opt = len(sys.argv[1::])
-if num_opt != 1:
-    raise DeepLCLIArgCheckingError('num of option is wrong(given %d, expected 1).'%num_opt)
-# raise err if specify 2 langs is empty
-opt_lang = sys.argv[1].split(':')
-if len(opt_lang) != 2 or opt_lang[0] not in fr_langs or opt_lang[1] not in to_langs:
-    raise DeepLCLIArgCheckingError('correct your lang format.')
-# raise err if <fr:lang> == <to:lang>
-fr_lang, to_lang = opt_lang
-if fr_lang == to_lang:
-    raise DeepLCLIArgCheckingError('two languages cannot be same.')
-# raise err if stdin > 5000 chr
+    def validate(self):
+        # <fr:lang> ::= {auto, ja, en, de, fr, es, pt, it, nl, pl, ru, zh}
+        fr_langs = {'auto', 'ja', 'en', 'de', 'fr', 'es', 'pt', 'it', 'nl', 'pl', 'ru', 'zh'}
+        # <to:lang> ::= <fr:lang> - {auto}
+        to_langs = fr_langs - {'auto'}
+        # if `$ deepl-cli`
+        if sys.stdin.isatty() and len(sys.argv) == 1:
+            self.usage()
+            exit(0)
+        # raise err if stdin is empty
+        if sys.stdin.isatty():
+            raise DeepLCLIArgCheckingError('stdin seems to be empty.')
+        # raise err if arity != 1
+        num_opt = len(sys.argv[1::])
+        if num_opt != 1:
+            raise DeepLCLIArgCheckingError('num of option is wrong(given %d, expected 1).'%num_opt)
+        # raise err if specify 2 langs is empty
+        opt_lang = sys.argv[1].split(':')
+        if len(opt_lang) != 2 or opt_lang[0] not in fr_langs or opt_lang[1] not in to_langs:
+            raise DeepLCLIArgCheckingError('correct your lang format.')
+        # raise err if <fr:lang> == <to:lang>
+        fr_lang, to_lang = opt_lang
+        if fr_lang == to_lang:
+            raise DeepLCLIArgCheckingError('two languages cannot be same.')
+        # raise err if stdin > 5000 chr
+        scripts = sys.stdin.read()
+        if len(scripts) > 5000:
+            raise DeepLCLIArgCheckingError('limit of script is less than 5000 chars(Now: ).'%len(scripts))
 
-scripts = sys.stdin.read()
-if len(scripts) > 5000:
-    raise DeepLCLIArgCheckingError('limit of script is less than 5000 chars(Now: ).'%len(scripts))
+        self.fr_lang = fr_lang
+        self.to_lang = to_lang
+        self.scripts = scripts
 
-# else input
+    def translate(self):
+        o = Options()
+        o.add_argument('--headless')
+        o.add_argument('--disable-gpu')
+        o.add_argument('--user-agent='\
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) '\
+            'AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0 Mobile/14C92 Safari/602.1'
+        )
+        d = webdriver.Chrome(options=o)
+        d.get('https://www.deepl.com/translator#%s/%s'%(self.fr_lang, self.to_lang))
+        # input: scripts
+        time.sleep(3)
+        i_xpath = '//textarea[@dl-test="translator-source-input"]'
+        input_textarea = d.find_element_by_xpath(i_xpath)
+        input_textarea.send_keys(self.scripts)
+        time.sleep(10)
+        o_xpath = '//textarea[@dl-test="translator-target-input"]'
+        res = d.find_element_by_xpath(o_xpath).get_attribute('value')
+        d.quit()
+        return res.rstrip()
 
-o = Options()
-o.add_argument('--headless')
-o.add_argument('--disable-gpu')
-o.add_argument('--user-agent='\
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) '\
-    'AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0 Mobile/14C92 Safari/602.1'
-)
+def main():
+    t = DeepLCLI()
+    t.validate()
+    print(t.translate())
 
-d = webdriver.Chrome(options=o)
-d.get('https://www.deepl.com/translator#%s/%s'%(fr_lang, to_lang))
-# input: scripts
-time.sleep(3)
-i_xpath = '//textarea[@dl-test="translator-source-input"]'
-input_textarea = d.find_element_by_xpath(i_xpath)
-input_textarea.send_keys(scripts)
-time.sleep(10)
-o_xpath = '//textarea[@dl-test="translator-target-input"]'
-res = d.find_element_by_xpath(o_xpath).get_attribute('value')
-print(res)
-d.quit()
+if __name__ == "__main__":
+    main()
