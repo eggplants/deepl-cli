5d2e7fe26a95c9ef230e84676ed521ef6444b4a1
haruna <w10776e8w@yahoo.co.jp>
2020-07-13 16:45:48 +0900

    hai

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='deepl-cli',
-    version='0.0.1',
+    version='0.0.2',
     description='DeepL Translator CLI using Selenium',
     description_content_type='',
     long_description=open('README.md').read(),
