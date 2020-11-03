e073b7dd6c2380573299df040ad260bcbd414764
eggplants <w10776e8w@yahoo.co.jp>
2020-11-03 13:36:39 +0900

    0.0.9

--- example.sh
+++ example.sh
@@ -7,3 +7,8 @@ curl -s http://example.com/ | grep -oP '(?<=>)[^<]+' | deepl en:ja
 curl -s https://www.w3.org/DesignIssues/LinkedData.html |
     tr -d \\n | grep -oP -m1 '(?<=p>)[^<]+(?=</p>)' |
     head -3 | sed 's/&nbsp;/ /g' | deepl :ja
+
+# translate the Announce on Codeforces
+curl -s 'https://codeforces.com/blog/entry/84257?locale=ru' |
+    grep -m5 ttypography | grep -oP '(?<=>)[^<]+' >codeforces_681
+deepl ru:en <codeforces_681
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='deepl-cli',
-    version='0.0.8',
+    version='0.0.9',
     description='DeepL Translator CLI using Selenium',
     description_content_type='',
     long_description=open('README.md').read(),
