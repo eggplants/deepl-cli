d504ede0d15f3776d9d5c644e8ee37c789d7d076
eggplants <w10776e8w@yahoo.co.jp>
2022-03-19 15:48:31 +0900

    fix: test

--- test_deepl.py
+++ test_deepl.py
@@ -1,53 +1,52 @@
 import pytest
 
-from deepl import deepl
+from deepl import DeepLCLI, DeepLCLIError
 
 
 def test1() -> None:
-    t = deepl.DeepLCLI(langs=("en", "ja"))
+    t = DeepLCLI("en", "ja")
     assert t.translate("hello") == "こんにちわ"
 
 
 def test2() -> None:
-    t = deepl.DeepLCLI(langs=("en", "ja"))
-    with pytest.raises(deepl.DeepLCLIArgCheckingError):
+    t = DeepLCLI("en", "ja")
+    with pytest.raises(DeepLCLIError):
         t.translate("\n")
 
 
 def test3() -> None:
-    t = deepl.DeepLCLI(langs=("en", "ja"))
-    with pytest.raises(deepl.DeepLCLIArgCheckingError):
-        t.chk_cmdargs()
+    with pytest.raises(DeepLCLIError):
+        DeepLCLI("en", "jaa")
 
 
 def test4() -> None:
-    t = deepl.DeepLCLI()
-    with pytest.raises(AttributeError):
-        t.translate("test")
+    with pytest.raises(DeepLCLIError):
+        DeepLCLI("enn", "ja")
 
 
 def test5() -> None:
-    with pytest.raises(deepl.DeepLCLIArgCheckingError):
-        deepl.DeepLCLI(langs=("en", "jaa"))
+    with pytest.raises(DeepLCLIError):
+        DeepLCLI("", "ja")
 
 
 def test6() -> None:
-    with pytest.raises(deepl.DeepLCLIArgCheckingError):
-        deepl.DeepLCLI(langs=("enn", "ja"))
+    with pytest.raises(DeepLCLIError):
+        DeepLCLI("enn", "jaa")
 
 
 def test7() -> None:
-    t = deepl.DeepLCLI(langs=("", "ja"))
-    assert t.translate("test") == "テスト"
+    t = DeepLCLI("auto", "ja")
+    with pytest.raises(DeepLCLIError):
+        t.translate("test" * 10000)
 
 
 def test8() -> None:
-    t = deepl.DeepLCLI(langs=("ja", "de"))
+    t = DeepLCLI("ja", "de")
     assert t.translate("今日は2022/2/22です。") == "Heute ist der 22.2.2022."
 
 
 def test9() -> None:
-    t = deepl.DeepLCLI(langs=("auto", "ja"))
+    t = DeepLCLI("auto", "ja")
     assert t.translate("test") == "テスト"
     assert t.translated_fr_lang == "en"
     assert t.translated_to_lang == "ja"
