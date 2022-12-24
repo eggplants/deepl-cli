91e076d50468693df3b7930fe549a8d60e3c89b0
eggplants <w10776e8w@yahoo.co.jp>
2022-12-24 12:54:51 +0900

    update: example

--- example.sh
+++ example.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # translate a page content in English into Japanese
-curl -s http://example.com/ | grep -oP '(?<=>)[^<]+' | deepl en:ja
+curl -s http://example.com/ | grep -oP '(?<=>)[^<]+' | deepl --fr en --to ja -s
 
 # translate the article page about LOAD written by TBL
 curl -s https://www.w3.org/DesignIssues/LinkedData.html |
     tr -d \\n | grep -oP -m1 '(?<=p>)[^<]+(?=</p>)' |
-    head -3 | sed 's/&nbsp;/ /g' | deepl :ja
+    head -3 | sed 's/&nbsp;/ /g' | deepl --to ja -s
 
 # translate the Announce on Codeforces
 curl -s 'https://codeforces.com/blog/entry/84257?locale=ru' |
-    grep -m5 ttypography|sed '$!d' | grep -oP '(?<=>)[^<]+' >codeforces_681
-deepl ru:en <codeforces_681
+    grep -m5 ttypography|sed '$!d' | grep -oP '(?<=>)[^<]+' | deepl --fr ru --to en -s
