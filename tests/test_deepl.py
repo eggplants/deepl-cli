d826117372d5a4c5c2686eaa8d0e9e199910c702
eggplants <w10776e8w@yahoo.co.jp>
2023-04-05 21:51:54 +0900

    chore: test

--- tests/test_deepl.py
+++ tests/test_deepl.py
@@ -3,65 +3,55 @@ import pytest
 from deepl import DeepLCLI, DeepLCLIError
 
 
-@pytest.mark.xfail(reason="timeout")
-def test1() -> None:
+def test_en_to_ja() -> None:
     t = DeepLCLI("en", "ja", 100000)
-    assert t.translate("hello") == "ハロー"
+    assert t.translate("hello.") == "こんにちは"
 
 
-@pytest.mark.xfail(reason="timeout")
-def test2() -> None:
+def test_blank_script() -> None:
     t = DeepLCLI("en", "ja", 100000)
     with pytest.raises(DeepLCLIError):
         t.translate("\n")
 
 
-@pytest.mark.xfail(reason="timeout")
-def test3() -> None:
-    with pytest.raises(DeepLCLIError):
-        DeepLCLI("en", "jaa", 100000)
-
-
-@pytest.mark.xfail(reason="timeout")
-def test4() -> None:
+def test_invalid_input_lang() -> None:
     with pytest.raises(DeepLCLIError):
         DeepLCLI("enn", "ja", 100000)
 
 
-@pytest.mark.xfail(reason="timeout")
-def test5() -> None:
+def test_invalid_output_lang() -> None:
+    with pytest.raises(DeepLCLIError):
+        DeepLCLI("en", "jaa", 100000)
+
+
+def test_blank_input_lang() -> None:
     with pytest.raises(DeepLCLIError):
         DeepLCLI("", "ja", 100000)
 
 
-@pytest.mark.xfail(reason="timeout")
-def test6() -> None:
+def test_invalid_input_and_output_lang() -> None:
     with pytest.raises(DeepLCLIError):
         DeepLCLI("enn", "jaa", 100000)
 
 
-@pytest.mark.xfail(reason="timeout")
-def test7() -> None:
+def test_input_language_auto() -> None:
     t = DeepLCLI("auto", "ja", 100000)
     with pytest.raises(DeepLCLIError):
         t.translate("test" * 10000)
 
 
-@pytest.mark.xfail(reason="timeout")
-def test8() -> None:
+def test_ja_to_de() -> None:
     t = DeepLCLI("ja", "de", 100000)
     assert t.translate("今日は2022/2/22です。") == "Heute ist der 22.2.2022."
 
 
-@pytest.mark.xfail(reason="timeout")
-def test9() -> None:
+def test_lang_attrs() -> None:
     t = DeepLCLI("auto", "ja", 100000)
     assert t.translate("test") == "試練"
     assert t.translated_fr_lang == "en"
     assert t.translated_to_lang == "ja"
 
 
-@pytest.mark.xfail(reason="timeout")
 def test10() -> None:
     t = DeepLCLI("en", "ja", 100000, use_dom_submit=True)
-    assert t.translate("hello") == "ハロー"
+    assert t.translate("hello.") == "こんにちは"
