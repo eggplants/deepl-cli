aa630ab6f9a298e3daa409422752edf331d9bb16
Nikolay Borodin <monsterovich@gmail.com>
2023-10-25 18:27:30 +0200

    fix: added more languages (to_langs)

--- deepl/deepl.py
+++ deepl/deepl.py
@@ -54,7 +54,12 @@ class DeepLCLI:
         "uk",
         "zh",
     }
-    to_langs = fr_langs - {"auto"}
+    to_langs = fr_langs | {
+        "en-US",
+        "en-GB",
+        "nb",
+        "pt-BR"
+    } - {"auto"}
 
     def __init__(
         self,
