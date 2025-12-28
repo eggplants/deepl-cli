4ba59497c79f7a2e030f76b5b971bf26af4ff61f
eggplants <w10776e8w@yahoo.co.jp>
2025-12-28 19:21:02 +0900

    update

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -exo pipefail
 
-uv sync --dev --all-extras
-uv run pre-commit install --allow-missing-config
+if ! grep -q mise ~/.zshrc; then
+  # shellcheck disable=SC2016
+  echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
+fi
+
+mise trust
+mise install
+mise generate git-pre-commit --write
+eval "$(mise activate zsh)"
 
 cat<<'A'>> ~/.zshrc
 eval "$(uv generate-shell-completion zsh)"
 eval "$(uvx --generate-shell-completion zsh)"
-command -v deactivate &>/dev/null || . .venv/bin/activate
 A
 
 sed -i ~/.zshrc -e 's/^ZSH_THEME=.*/ZSH_THEME="refined"/'
-
-[[ -d .venv ]] || uv venv
